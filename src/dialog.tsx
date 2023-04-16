9bb4e29db2dff7bff755a184a8d28894e2a908c2
algutifana <aaron.algutifan@gmail.com>
2023-04-16 01:28:38 -0500

    added dialog window & now show results

---
+++ src/dialog.tsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import CircularProgress from '@mui/material/CircularProgress';
+import { userFound } from './getStarted';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import CheckIcon from '@mui/icons-material/Check';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { Avatar, Grid } from '@mui/material';
+import { OptOut } from './getStarted';
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+interface FullScreenDialogProps {
+    open: boolean;
+    spinner: boolean;
+    data: userFound[];
+    optouts: OptOut[];
+    closeDialog: () => void;
+  }
+  
+  export default function DialogWindow({ open, data, spinner, optouts, closeDialog }: FullScreenDialogProps) {
+
+  const findOptOut = (hostname: string) => {
+    const found = optouts.find((optout) => optout.url.includes(hostname));
+    if (found) {
+      return found.url;
+    }
+    return "";
+  }
+  const handleClose = () => {
+    closeDialog();
+  };
+
+  return (
+    <div>
+      <Dialog
+        
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              {spinner? "Loading..." : "Results"} 
+            </Typography>
+            &nbsp; 
+            {spinner?
+          <CircularProgress color="secondary" /> 
+          
+          : <></>}
+          </Toolbar>
+        </AppBar>
+
+        <Grid container spacing={2} alignItems = "center" justifyContent="center" sx ={{ display: 'flex', marginTop: 1, marginBottom: 1}}>
+      {data.map(res => (
+        <Grid item xs={12} key={res.hostname}>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item>
+            {(res.found == '1')? <PriorityHighIcon color='error'></PriorityHighIcon> : <CheckIcon color = 'success'></CheckIcon>}
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">{res.hostname}</Typography>
+            </Grid>
+            <Grid item>
+            {(res.found == '1')? <Button variant="outlined" href = {findOptOut(res.hostname)} color="primary">Opt Out</Button> : <></>}
+            </Grid>
+          </Grid>
+        </Grid>
+      ))}
+    </Grid>
+      </Dialog>
+    </div>
+  );
+}
--- src/getStarted.tsx
+++ src/getStarted.tsx
@@ -1,11 +1,22 @@
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react';
-
+import DialogWindow from './dialog';
 import autoComplete from './autoComplete';
 import { UserContext } from './context';
+
+export interface userFound {
+    hostname: string;
+    found: string;
+}
+
+export interface OptOut{
+  linknumber: string;
+  url: string; 
+}
+
 
 function checkFullName(fullName: string) {
     const nameRegex = /^[a-zA-Z\s]+$/; // Only letters and spaces
@@ -30,7 +41,43 @@ function checkFullName(fullName: string) {
 export default function GetStarted(){
     
     const { name, setName, cityState } = useContext(UserContext);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [showSpinner, setShowSpinner] = useState(false);
+    const [results, setResults] = useState<userFound[]>([]);
+    const [optoutLinks, setOptOutLinks] = useState<OptOut[]>([]);
 
+
+    useEffect(() => {
+      fetch('https://us-central1-optuout-e7ffe.cloudfunctions.net/app/graphql', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                query: `
+                  query {
+                    optOuts {
+                      linknumber
+                      url
+                    }
+                  }
+                `
+              })
+            })
+            .then(res => res.json())
+            .then(data => setOptOutLinks(data.data.optOuts))
+            .catch(error => console.error(error));
+    
+      return () => {
+        console.log('done')
+      }
+    }, [])
+    
+    const handleOpenDialog = () => {
+      setDialogOpen(true);
+      
+  };
+    const handleCloseDialog = () => {
+      setDialogOpen(false);
+  };
 
     const handleChange = (event: any) => {
         setName(event.target.value);
@@ -38,7 +85,6 @@ export default function GetStarted(){
 
  
     function submit(){
-
         var trimmedCityState = cityState.split(', ')
 
         if (!trimmedCityState.includes('USA')){
@@ -51,39 +97,50 @@ export default function GetStarted(){
             return 
         }
         else if (trimmedCityState.length == 3){
+            setResults([] as userFound[])
 
             let firstname = name.split(' ')[0];
             let lastname = name.split(' ')[1];
             let city = trimmedCityState[0];
             let state = trimmedCityState[1];
-            console.log(city + " " + state)
 
-            fetch("https://navy-blue-moth-shoe.cyclic.app/checksites", {
+            const variables = { first: firstname, last: lastname, city: city, state: state };
+            const query = `
+              mutation CheckSites($first: String!, $last: String!, $city: String!, $state: String!) {
+                checkSites(first: $first, last: $last, city: $city, state: $state) {
+                  hostname
+                  found
+                }
+              }
+            `;
+
+            const body = JSON.stringify({ query, variables });
+
+            setShowSpinner(true);
+            
+
+
+            fetch("https://us-central1-optuout-e7ffe.cloudfunctions.net/app/graphql", {
                 method: 'POST',
                 headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json'
                 },
 
-                body: JSON.stringify({
-
-                  user: {
-                    first: firstname, 
-                    last: lastname, 
-                    city: city, 
-                    state: state
-                  }
-                
-                }),
+                body: body
                 })
             .then(function(response) {
                 if(response.ok){
+                    setShowSpinner(false);
                     return response.json();
+                    
                 }{
                     throw new Error("Post Failed")
                 }
             }).then(function(responseBody){
-                console.log(responseBody.uri)
+              const checkSites = responseBody.data.checkSites;
+              console.log(checkSites)
+              setResults(checkSites);
             })
             .catch(function(error) {
                 console.log("Request failed", error);
@@ -95,7 +152,7 @@ export default function GetStarted(){
     }
 
     return (
-
+      <div>
     <Stack direction ="row" spacing = {2} style={{ marginTop: '25px', alignItems: "center", justifyContent: "center"}}>
         
         <TextField 
@@ -114,11 +171,32 @@ export default function GetStarted(){
         variant = "contained" 
         color = "secondary"
         style = {{zIndex: 10}}
-        onClick = {submit}
+        onClick={() => {
+          submit();
+          handleOpenDialog();
+        }}
         >
             Get Started
             </Button>
+
+            
+
+            
+
+            <DialogWindow optouts = {optoutLinks} data = {results} open={dialogOpen} spinner = {showSpinner} closeDialog={handleCloseDialog} />
+
     </Stack>
+    {results.length > 1 && <Button 
+        variant = "contained" 
+        color = "secondary"
+        style = {{width: 250 ,zIndex: 10}}
+        onClick={() => {
+          handleOpenDialog()
+        }}
+        >
+            Open Results
+            </Button>}
+    </div>
     )
 }
 
